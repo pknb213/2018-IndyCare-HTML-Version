0c494f7a4294eaed292bb31c9f9de15f3c0c0a8e
pknb213 <pknb321@gmail.com>
2019-09-11 18:41:22 +0900

    Test Clip

--- apis.py
+++ apis.py
@@ -1,18 +1,66 @@
 from utils import *
 
+# cache.hset(sn, 'ts', datetime.datetime.now().timestamp())
+# cam = True
+#
+# if cache.hget(sn, 'clip') is None:  # 클립이 없으면, 아래로
+#     cache.hset(sn, 'clip', 0 if cam else -1)  # cam 값이 있으면 0, 없으면 -1
 
-@app.route("/clip/check")
-def clip_check():
+
+@app.route("/clip/<sn>/check")
+def clip_check(sn):
     res = 'ok'
     #res = 'ready'
     # res = '1'
+
+    # sn (clip) set -1 :  -1은 file이 없을 떄 (None)
+    file_name = 'Chronograf.mp4'
+    clip_path = os.path.join(os.getcwd(), 'static', file_name)
+    print(clip_path)
+    # added file.save
+    cache.hset(sn, 'clipname', file_name)
+    print("hset : ", sn, 'clipname', file_name)
+    time.sleep(0.02)  # for scheduling
+    cache.hset(sn, 'clip', datetime.now().timestamp())
+    print("hset : ", sn, 'clip', datetime.now().timestamp())
+
+    time.sleep(10)
+
+    clip = cache.hget(sn, 'clip')
+    if float(clip) < 0:
+        cache.hset(sn, 'clip', 0)
+        print("No Camera : sn (clip) set 0")
+    else:
+        print("Check OK")
+
     return res
 
 
-@app.route("/clip/<cam>")
-def clip_cam(cam):
-    print(cam)
-    return send_from_directory(os.path.join(os.getcwd(), 'static'), 'Chronograf.mp4')
+@app.route("/clip/<sn>/<cam>")
+def clip_cam(sn, cam):
+    print(sn, cam)
+    clip_path = os.path.join(os.getcwd(), 'static', cache.hget(sn, 'clipname').decode('utf-8'))
+    print("Clip Path : ", clip_path)
+
+    t0 = t1 = datetime.now()
+    cache.hset(sn, 'clip', 0)  # 클립이 0이면 Waiting 반복, -1이면 Fail, 0보다 크면 영상 존재
+    while t1.timestamp() - t0.timestamp() <= app.config['ROBOT_DATA_WAIT_TIMEOUT']:
+        st = float(cache.hget(sn, 'clip'))
+
+        if st < 0:
+            print("Fail")
+            return Response("Fail")
+        if st > 0:
+            print("Succ")
+            print(cache.keys())
+            print(cache.hgetall('D1234'))
+            return send_from_directory(os.path.join(os.getcwd(), 'static'), 'Chronograf.mp4')
+
+        time.sleep(1)
+        t1 = datetime.now()
+        print('Clip Waiting', t1.timestamp() - t0.timestamp())
+
+    return Response("Error")
 
 
 '''
--- python/utils.py
+++ python/utils.py
@@ -1,10 +1,19 @@
 import os, sys
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, Response
+import time
+from datetime import datetime
+from redis import Redis, RedisError
 
 
 app = Flask(__name__, template_folder=os.getcwd()+'/templates', static_folder=os.getcwd()+'/static')
 app.config.update(
     DEBUG=False,
     SCREATE_KEY='secret_xxx',
+    ROBOT_DATA_WAIT_TIMEOUT = 10
 )
 
+cache = Redis("localhost")
+cache.flushdb()
+print(cache.keys())
+print(cache.hkeys('D11924I07T02'))
+print(cache.hgetall('D11924I07T02'))
